466b5893b188d0936f711d8a88114ee5d94415d6
Michael Mathews <michaelmathews@AGI095.fios-router.home>
2020-10-23 12:49:51 -0400

    commit

--- qb_arm.py
+++ qb_arm.py
@@ -23,29 +23,54 @@ jared_goff = speed[speed.QB == 'Jared Goff']
 jared_goff_plot = px.line(jared_goff, x='frame_id', y='speed',
         title='Speed of the Football on Jared Goff Passing Plays - Week 17, 2018')
 
+homepage_image = Image.open('al-soot-c3SY0VqneSg-unsplash.jpg')
+josh_allen_image = Image.open('bills.jpg')
+matthew_stafford_image = Image.open('josh-garcia-iqa83PdCO1A-unsplash.jpg')
+jared_goff_image = Image.open('bp-miller-mu2tDeICLNI-unsplash.jpg')
 
-image = Image.open('al-soot-c3SY0VqneSg-unsplash.jpg')
-st.image(image, use_column_width=True)
+def main():
+    page = st.sidebar.selectbox('Select Quarterback', ('Home Page', 'Josh Allen', 'Matthew Stafford', 'Jared Goff'))
 
-def get_qb(name):
-    if name =='Josh Allen':
+    if page == 'Home Page':
+        st.image(homepage_image, use_column_width=True)
+        st.title('QB Throwing Power - Week 17, 2018')
+        st.write('''
+        something,  something  something,  something,  something,  something,  something,  something,  something
+        ''')
+    elif page == 'Josh Allen':
+        st.title('Josh ALlen Throwing Power - Week 17, 2018')
+        st.image(josh_allen_image, use_column_width=True)
+        st.write('''
+                ### How Do I Read This Chart?
+                The line chart below tracks the speed (in miles per hour) of the football, frame by frame during a play.
+                The data aggregates to the "max" speed of the football at each frame. For example, in week 17 of the
+                2018 NFL football season, Josh Allen dropped back to throw the football 28 times. On one play, in the 41st
+                frame of the play, the football was moving at 27.36 MPH.
+                ''')
         st.write(josh_allen_plot)
-    elif name == 'Matthew Stafford':
+    elif page == 'Matthew Stafford':
+        st.title('Matthew Stafford Throwing Power - Week 17, 2018')
+        st.image(matthew_stafford_image, use_column_width=True)
+        st.write('''
+                ### How Do I Read This Chart?
+                The line chart below tracks the speed (in miles per hour) of the football, frame by frame during a play.
+                The data aggregates to the "max" speed of the football at each frame. For example, in week 17 of the
+                2018 NFL football season, Matthew Stafford dropped back to throw the football 28 times. On one play, in the 42nd
+                frame of the play, the football was moving at 25.12 MPH.
+                ''')
         st.write(matthew_stafford_plot)
-    elif name == 'Jared Goff':
+    elif page == 'Jared Goff':
+        st.title('Jared Goff Throwing Power - Week 17, 2018')
+        st.image(jared_goff_image, use_column_width=True)
+        st.write('''
+                ### How Do I Read This Chart?
+                The line chart below tracks the speed (in miles per hour) of the football, frame by frame during a play.
+                The data aggregates to the "max" speed of the football at each frame. For example, in week 17 of the
+                2018 NFL football season, Jared Goff dropped back to throw the football 28 times. On one play, in the 40th
+                frame of the play, the football was moving at 25.85 MPH.
+                ''')
         st.write(jared_goff_plot)
-    return select_qb
-
-select_qb = st.sidebar.selectbox('Select Quarterback', ('Josh Allen', 'Matthew Stafford', 'Jared Goff'))
 
 
-st.title('QB Throwing Power - Week 17, 2018')
-st.write('''
-### How Do I Read This Chart?
-The line chart below tracks the speed (in miles per hour) of the football, frame by frame during a play.
-The data aggregates to the "max" speed of the football at each frame. For example, in week 17 of the
-2018 NFL football season, Josh Allen dropped back to throw the football 28 times. On one play, in the 41st
-frame of the play, the football was moving at 27.36 MPH.
-''')
-st.write(get_qb(select_qb))
+st.write(main())
 
